f135e2ef84fa52ab0c572773084b9c79ee60a3d6
Hatrickek <halimbayka@outlook.com>
2025-10-17 21:41:26 +0300

    disable thumbnails by default for now

--- OxylusEditor/src/Utils/EditorConfig.hpp
+++ OxylusEditor/src/Utils/EditorConfig.hpp
@@ -17,7 +17,7 @@ inline AutoCVar_Float cvar_camera_sens("editor.camera_sens", "editor camera sens
 inline AutoCVar_Int cvar_camera_smooth("editor.camera_smooth", "editor camera smoothing", 1);
 inline AutoCVar_Int cvar_camera_zoom("editor.camera_zoom", "editor camera zoom for ortho projection", 1);
 
-inline AutoCVar_Int cvar_file_thumbnails("editor.file_thumbnails", "show file thumbnails in content panel", 1);
+inline AutoCVar_Int cvar_file_thumbnails("editor.file_thumbnails", "show file thumbnails in content panel", 0);
 inline AutoCVar_Float
   cvar_file_thumbnail_size("editor.file_thumbnail_size", "file thumbnail size in content panel", 120.0f);
 inline AutoCVar_Int cvar_show_meta_files("editor.show_meta_files", "show oxasset files in conten panel", 1);
