1288c088da85be8e4fcfe98f2c3d8500d3135dbe
hatrickek <halimbayka@outlook.com>
2025-12-21 20:06:21 +0300

    embedded imgui.slang

--- Oxylus/include/Asset/EmbedAsset.hpp
+++ Oxylus/include/Asset/EmbedAsset.hpp
@@ -1,10 +1,16 @@
 ﻿#pragma once
 
 #include <string>
+#include <vuk/runtime/vk/VkRuntime.hpp>
 
 namespace ox {
 class EmbedAsset {
 public:
-  static void EmbedTexture(const std::string& texFilePath, const std::string& outPath, const std::string& arrayName);
+  static auto embed_texture(
+    const std::string& tex_file_path, const std::string& out_path, const std::string& array_name
+  ) -> void;
+  static auto embed_shader(vuk::Runtime& runtime, const std::string& shader_path, const std::string& out_path, const std::string& entry_point)
+    -> void;
+  static auto embed_binary(const std::string& binary_path, const std::string& out_path, const std::string& array_name) -> void;
 };
 } // namespace ox
