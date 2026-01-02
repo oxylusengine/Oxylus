4bba611ee2ca1f68e157fac3922e7059c6d1dd9e
exdal <63502313+exdal@users.noreply.github.com>
2026-01-02 17:03:10 +0300

    remove image.slang and atomic.slang

--- shell.nix
+++ shell.nix
@@ -1,44 +1,60 @@
 let
   pkgs = import <nixpkgs> { };
 in pkgs.mkShell.override {
-  stdenv = pkgs.llvmPackages_21.libcxxStdenv;
+  stdenv = pkgs.llvmPackages_latest.libcxxStdenv;
 } {
   nativeBuildInputs = [
+    pkgs.xmake
     pkgs.cmake
     pkgs.ninja
-    pkgs.gnumake
-    pkgs.xmake
+    pkgs.pkg-config
 
-    pkgs.llvmPackages_21.bintools-unwrapped
-    pkgs.llvmPackages_21.libcxx
-    pkgs.llvmPackages_21.libcxx.dev
-    pkgs.llvmPackages_21.compiler-rt
-    (pkgs.llvmPackages_21.clang-tools.override {
-      enableLibcxx = true;
-    })
     pkgs.mold
 
-    pkgs.pkg-config
+    pkgs.llvmPackages_latest.libcxx
+    pkgs.llvmPackages_latest.libcxx.dev
+    pkgs.llvmPackages_latest.compiler-rt
+    pkgs.llvmPackages_latest.bintools-unwrapped
+    (pkgs.llvmPackages_latest.clang-tools.override {
+      enableLibcxx = true;
+     })
+
     pkgs.python313
     pkgs.python313Packages.pip
     pkgs.python313Packages.setuptools
     pkgs.python313Packages.wheel
 
-    pkgs.zlib.dev
-
-    # for gltfpack
     pkgs.meshoptimizer
 
-    # for SDL3
-    pkgs.sdl3
+    # SDL3
+    pkgs.xorg.libX11
+    pkgs.xorg.libxcb
+    pkgs.xorg.libXScrnSaver
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXi
+    pkgs.xorg.libXrandr
+
+    pkgs.vulkan-loader
   ];
 
-  shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.llvmPackages_21.libcxx}/lib:$LD_LIBRARY_PATH
-    # slang needs libstdc++
-    export LD_LIBRARY_PATH=${pkgs.gcc14.cc.lib}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${pkgs.lua5_3_compat}/lib:$LD_LIBRARY_PATH
-  '';
+  LIBCXX_PATH="${pkgs.llvmPackages_latest.libcxx.dev}";
+  LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+    pkgs.llvmPackages_latest.libcxx
+    pkgs.gcc14.cc.lib
+    pkgs.vulkan-loader
+    # SDL3
+    pkgs.xorg.libX11
+    pkgs.xorg.libxcb
+    pkgs.xorg.libXScrnSaver
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXi
+    pkgs.xorg.libXrandr
+  ]}";
+  NIX_ENFORCE_NO_NATIVE = "0";
 
   hardeningDisable = [ "all" ];
 }
