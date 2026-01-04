1d12ddcb5f3f048b442ab4a4d83e2fae709dd2ec
Hatrickek <halimbayka@outlook.com>
2026-01-04 22:44:46 +0300

    improved notification rendering

--- OxylusEditor/src/Editor.cpp
+++ OxylusEditor/src/Editor.cpp
@@ -131,10 +131,30 @@ auto Editor::init(this Editor& self) -> std::expected<void, std::string> {
     loguru::Verbosity_INFO
   );
 
+  self.runtime_console
+    .register_command("test_loading", "Test Loading.", [&self](const RuntimeConsole::ParsedCommandValue&) {
+      self.notification_system.add(Notification("Test Loading", true, Notification::Type::Loading));
+    });
+
+  self.runtime_console.register_command("test_info", "Test Info.", [&self](const RuntimeConsole::ParsedCommandValue&) {
+    self.notification_system.add(Notification("Test Info", true, Notification::Type::Info));
+  });
+
+  self.runtime_console
+    .register_command("test_warn", "Test Warning.", [&self](const RuntimeConsole::ParsedCommandValue&) {
+      self.notification_system.add(Notification("Test Warning", true, Notification::Type::Warn));
+    });
+
+  self.runtime_console.register_command("test_err", "Test Error.", [&self](const RuntimeConsole::ParsedCommandValue&) {
+    self.notification_system.add(Notification("Test Error", true, Notification::Type::Error));
+  });
+
   return {};
 }
 
 auto Editor::deinit(this Editor& self) -> std::expected<void, std::string> {
+  ZoneScoped;
+
   auto& job_man = App::get_job_manager();
   job_man.get_tracker().stop_tracking();
 
@@ -184,6 +204,8 @@ auto Editor::update(this Editor& self, const Timestep& timestep) -> void {
 }
 
 auto Editor::render(this Editor& self, const vuk::ImageAttachment& swapchain_attachment) -> void {
+  ZoneScoped;
+
   auto& job_man = App::get_job_manager();
 
   auto status = job_man.get_tracker().get_status();
@@ -251,6 +273,8 @@ auto Editor::render(this Editor& self, const vuk::ImageAttachment& swapchain_att
 }
 
 void Editor::reset(this Editor& self) {
+  ZoneScoped;
+
   self.main_viewport_panel.reset();
 
   auto* sh = self.get_panel<SceneHierarchyPanel>();
@@ -260,6 +284,8 @@ void Editor::reset(this Editor& self) {
 }
 
 void Editor::new_scene(this Editor& self) {
+  ZoneScoped;
+
   App::get_vkcontext().wait();
 
   auto new_scene_id = self.scene_manager.new_scene();
@@ -270,6 +296,8 @@ void Editor::new_scene(this Editor& self) {
 }
 
 bool Editor::open_scene(const std::filesystem::path& path) {
+  ZoneScoped;
+
   auto loaded_scene = scene_manager.load_scene(path);
 
   if (loaded_scene.has_value()) {
@@ -281,6 +309,8 @@ bool Editor::open_scene(const std::filesystem::path& path) {
 }
 
 void Editor::open_scene_file_dialog() {
+  ZoneScoped;
+
   const auto& window = App::get_window();
   FileDialogFilter dialog_filters[] = {{.name = "Oxylus scene file(.oxscene)", .pattern = "oxscene"}};
   window.show_dialog({
@@ -307,6 +337,8 @@ void Editor::open_scene_file_dialog() {
 }
 
 void Editor::save_scene() {
+  ZoneScoped;
+
   auto* focused_viewport = main_viewport_panel.get_focused_viewport();
   if (!focused_viewport)
     return;
@@ -331,6 +363,8 @@ void Editor::save_scene() {
 }
 
 void Editor::save_scene_as() {
+  ZoneScoped;
+
   auto* focused_viewport = main_viewport_panel.get_focused_viewport();
   if (!focused_viewport)
     return;
@@ -384,6 +418,8 @@ void Editor::save_scene_as() {
 }
 
 void Editor::editor_shortcuts() {
+  ZoneScoped;
+
   auto& input_sys = App::mod<Input>();
   if (input_sys.get_action_pressed("new_scene")) {
     new_scene();
@@ -406,6 +442,8 @@ void Editor::editor_shortcuts() {
 }
 
 void Editor::set_docking_layout(EditorLayout layout) {
+  ZoneScoped;
+
   current_layout = layout;
   ImGui::DockBuilderRemoveNode(dockspace_id);
   ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_PassthruCentralNode);
@@ -439,12 +477,16 @@ void Editor::set_docking_layout(EditorLayout layout) {
 }
 
 void Editor::reset_current_docking_layout() {
+  ZoneScoped;
+
   set_docking_layout(current_layout);
 
   main_viewport_panel.update_dockspace();
 }
 
 void Editor::draw_menubar() {
+  ZoneScoped;
+
   if (ImGui::BeginMenuBar()) {
     if (ImGui::BeginMenu("File")) {
       ImGui::Separator();
@@ -480,6 +522,7 @@ void Editor::draw_menubar() {
       ImGui::MenuItem("Inspector", nullptr, &get_panel<InspectorPanel>()->visible);
       ImGui::MenuItem("Scene hierarchy", nullptr, &get_panel<SceneHierarchyPanel>()->visible);
       ImGui::MenuItem("Console window", nullptr, &runtime_console.visible);
+      ImGui::MenuItem("Text Editor", nullptr, &get_panel<TextEditorPanel>()->visible);
       if (ImGui::BeginMenu("Layout")) {
         if (ImGui::MenuItem("Classic")) {
           set_docking_layout(EditorLayout::Classic);
@@ -496,7 +539,6 @@ void Editor::draw_menubar() {
       if (ImGui::MenuItem("Asset Manager")) {
         get_panel<AssetManagerPanel>()->visible = true;
       }
-      UI::tooltip_hover("WIP");
       ImGui::EndMenu();
     }
     if (ImGui::BeginMenu("Help")) {
