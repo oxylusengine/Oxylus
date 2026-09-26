93e51bd5a03f7ed75fdd94f552a6f9c95ce64f14
exdal <63502313+exdal@users.noreply.github.com>
2026-09-26 15:41:18 +0300

    support renderless app

--- Oxylus/src/Core/App.cpp
+++ Oxylus/src/Core/App.cpp
@@ -87,8 +87,10 @@ auto App::init(this App& self) -> void {
 }
 
 auto App::step(this App& self) -> void {
-  const i32 frame_limit = self.frame_limit > 0 ? self.frame_limit
-                                               : self.render_context->context_cvar.cvar_frame_limit.get();
+  auto frame_limit = self.frame_limit;
+  if (frame_limit <= 0 && self.render_context) {
+    frame_limit = self.render_context->context_cvar.cvar_frame_limit.get();
+  }
   if (frame_limit > 0) {
     self.timestep.set_max_frame_time(1000.0 / static_cast<f64>(frame_limit));
   } else {
